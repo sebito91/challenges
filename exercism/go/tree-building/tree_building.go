5b7f31fdced87ed7de9e15b57048e3df994d370e
Sebastian Borza <sebito91@gmail.com>
2021-04-09 00:08:18 -0500

    refactor to improve slightly

--- exercism/go/tree-building/tree_building.go
+++ exercism/go/tree-building/tree_building.go
@@ -16,85 +16,58 @@ type Node struct {
 	Children []*Node
 }
 
-// Mismatch is a placeholder variable
-type Mismatch struct{}
-
-func (m Mismatch) Error() string {
-	return "c"
-}
-
 // declare mappers to be usable across functions
 var mappers map[int][]*Node
 
-func checkRecord(record Record) error {
+func checkRecord(record Record, ids []bool) error {
+	if record.ID >= len(ids) || ids[record.ID] {
+		return fmt.Errorf("node already seen: %d", record.ID)
+	}
+
+	if record.ID <= record.Parent && record.ID != 0 {
+		return fmt.Errorf("parent cannot be greater than ID, parent: %d, ID: %d", record.Parent, record.ID)
+	}
+
 	if record.ID == 0 && record.Parent != 0 {
-		return Mismatch{}
-	}
-
-	if record.ID == record.Parent && record.ID != 0 {
-		return Mismatch{}
-	}
-
-	if record.Parent > record.ID {
-		return Mismatch{}
-	}
-
-	for _, child := range mappers[record.Parent] {
-		if record.ID == child.ID {
-			return Mismatch{}
-		}
+		return fmt.Errorf("root not cannot have a parent")
 	}
 
 	return nil
+
 }
 
 // Build stitches together the node tree
 func Build(records []Record) (*Node, error) {
-	var rootSent bool
 	mappers = make(map[int][]*Node)
 
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	ids := make([]int, len(records))
-	for idx, record := range records {
-		ids[idx] = record.ID
-		if record.Parent == 0 && record.ID == 0 {
-			if rootSent {
-				return nil, Mismatch{}
-			}
+	ids := make([]bool, len(records))
 
-			rootSent = true
-			continue
-		}
-
-		if err := checkRecord(record); err != nil {
+	for _, record := range records {
+		if err := checkRecord(record, ids); err != nil {
 			return nil, err
 		}
 
-		mappers[record.Parent] = append(mappers[record.Parent], &Node{ID: record.ID})
+		ids[record.ID] = true
+
+		if record.ID != 0 {
+			mappers[record.Parent] = append(mappers[record.Parent], &Node{ID: record.ID})
+		}
 	}
 
-	if !rootSent {
-		return nil, Mismatch{}
+	for idx, id := range ids {
+		if !id {
+			return nil, fmt.Errorf("did not receive expected id: %d", idx)
+		}
 	}
 
 	// generate the root
 	root := &Node{}
 
-	sort.Ints(ids)
-	if len(ids) == 1 {
-		return root, nil
-	}
-
-	for i := 1; i < len(ids); i++ {
-		if ids[i]-ids[i-1] > 1 {
-			return nil, fmt.Errorf("y")
-		}
-	}
-
-	parents := make([]int, 0, len(mappers))
+	parents := make([]int, len(mappers))
 	for parent := range mappers {
 		parents = append(parents, parent)
 		sort.Slice(mappers[parent], func(i, j int) bool {
