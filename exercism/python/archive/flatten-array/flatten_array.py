d870beb4b7d279d354662d8e79c5dc23d6fb30ef
Sebastian Borza <sebito91@gmail.com>
2023-04-27 16:15:54 -0500

    chore(python): update flatten-array solution

--- exercism/python/archive/flatten-array/flatten_array.py
+++ exercism/python/archive/flatten-array/flatten_array.py
@@ -1,13 +1,24 @@
-""" module to flatten a given array into its sub-elements """
+"""Module to flatten the list of lists."""
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
 
-def actually_flatten(array):
-    """ helper function to flatten the given array """
+
+def helper(lister):
+    """Generate the individual elements of the list of lists."""
+    if hasattr(lister, "__iter__"):
+        for each in lister:
+            if hasattr(each, "__iter__"):
+                for item in helper(each):
+                    yield item
+            else:
+                yield each
+    else:
+        yield lister
 
 
-def flatten(array):
-    """ function flatten our array """
-    return [x for x in actually_flatten(array) if x != [] and x is not None]
+def flatten(lister):
+    """Return the flatter list of lists."""
+    output = (item for each in lister for item in helper(each))
 
+    return [x for x in output if x is not None]
